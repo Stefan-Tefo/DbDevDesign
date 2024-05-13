8bd1de80c7ef59c897fdccfaf58fdf65e2d84a55
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-05-14 00:29:05 +0200

    Update homework-1.sql

--- Homework 1/homework-1.sql
+++ Homework 1/homework-1.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS student(
 	last_name VARCHAR(30) NULL,
 	date_of_birth TIMESTAMPTZ NOT NULL,
 	enrolled_date TIMESTAMPTZ NOT NULL,
-	gender ENUM("M","F") NOT NULL,
+	gender CHAR(1) NOT NULL,
 	national_id_number SMALLINT NOT NULL,
 	student_card_number SMALLINT NOT NULL
 );
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS teacher(
 	first_name VARCHAR(20) NULL,
 	last_name VARCHAR(30) NULL,
 	date_of_birth TIMESTAMPTZ NOT NULL,
-	academic_rank ENUM("student","teacher") NOT NULL,
+	academic_rank VARCHAR(20) NOT NULL,
 	hire_date TIMESTAMPTZ NOT NULL
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS grade(
 	student_id SMALLINT NOT NULL,
 	course_id SMALLINT NOT NULL,
 	teacher_id SMALLINT NOT NULL,
-	grade ENUM("A","B","C","D","F") NOT NULL,
+	grade SMALLINT NOT NULL,
 	comment VARCHAR(100) NULL,
 	create_date TIMESTAMPTZ NOT NULL
 );
