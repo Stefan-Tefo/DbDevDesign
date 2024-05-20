90a03295f3ab94febba98ad5f845e73ad3e97a7d
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-05-21 01:03:06 +0200

    Update homework-5.sql

---
+++ Homework 5/homework-5.sql
@@ -0,0 +1,5 @@
+-- • Create a function that returns all information from the artist details table for a given artist id, using a row variable.
+-- • Create a function doing the same thing as above but using a record.
+-- • Create a function that will return all rock songs, determined by the artist id, with all of the artist details.
+-- • Create a function that returns the quantity of songs for a given album ID, only if the quantity is bigger than 5. If it is not, raise a notice. Use the if/else statement.
+-- • Create a function that returns all artists that sang a song from a playlist with a certain id.
