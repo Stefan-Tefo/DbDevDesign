b7eb9d50d26308956f1339c7d37d31d74db59716
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-05-24 13:58:26 +0200

    Update homework-6.sql

--- Homework 6/homework-6.sql
+++ Homework 6/homework-6.sql
@@ -1,1 +1,16 @@
 -- • Create a stored procedure, called add_song_to_album, which, when called, will insert a new song into the song table, associate it with an album, and optionally insert lyrics.
+
+ CREATE OR REPLACE PROCEDURE add_song_to_album(
+ 	p_name VARCHAR,
+    p_duration INTERVAL,
+    p_explicit BOOLEAN
+ ) LANGUAGE PLPGSQL AS $$
+ DECLARE	
+    artist_id INT REFERENCES artist(id);
+    album_id INT REFERENCES album(id);
+ BEGIN
+ 	
+	INSERT INTO song(name) VALUES (p_name) RETURNING id INTO 
+ 
+ END;
+ $$;
